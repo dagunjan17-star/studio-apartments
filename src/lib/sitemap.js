1d697f7fdb6f3864f0a388dc5ad9972823ee240e
Mukesh Kumar <mk8701952@gmail.com>
2026-06-27 16:52:29 +0530

    auto update

--- src/lib/sitemap.js
+++ src/lib/sitemap.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
 
+import { locations } from "@/data/locations";
+const createSlug = (location) => {
+  return location
+    .replace(", Gurgaon", "")
+    .toLowerCase()
+    .replace(/,/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
 const currentDate =
     new Date().toISOString();
 
@@ -23,6 +32,17 @@ export async function generateSitemap() {
   `;
 
 
+   const locationUrls = locations.map((loc) => {
+    const slug = createSlug(loc);
+
+    return `
+      <url>
+        <loc>${baseUrl}/studio-apartments-in-${slug}-gurgaon</loc>
+         <lastmod>${currentDate}</lastmod>
+      </url>
+    `;
+  });
+  // 🔥 BLOG URLs
   let blogUrls = [];
 
   try {
@@ -62,6 +82,7 @@ export async function generateSitemap() {
   // 🔹 Combine all
   const allUrls = [
     staticUrls,
+    ...locationUrls,
      ...blogUrls,
   ].join("\n");
 
