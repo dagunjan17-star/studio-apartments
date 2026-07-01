6737e22d3c651bddbaeb3ce7f4d5e84a4b5b0310
Mukesh Kumar <mk8701952@gmail.com>
2026-07-01 13:31:35 +0530

    auto update

--- src/components/GurgaonRealEstateSection.jsx
+++ src/components/GurgaonRealEstateSection.jsx
@@ -68,8 +68,26 @@ const faqs = [
 
 export default function GurgaonRealEstateSection() {
   const [openIndex, setOpenIndex] = useState(0);
-
+const faqSchema = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  mainEntity: faqs.map((faq) => ({
+    "@type": "Question",
+    name: faq.question,
+    acceptedAnswer: {
+      "@type": "Answer",
+      text: faq.answer,
+    },
+  })),
+};
   return (
+    <>
+      <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(faqSchema),
+      }}
+    />
    <section className="relative overflow-hidden bg-white py-10 px-4 sm:px-6">
 
   {/* Background Lights */}
@@ -184,5 +202,6 @@ Studio apartments represent one of the fastest-growing residential property segm
     </div>
   </div>
 </section>
+</>
   );
 }
